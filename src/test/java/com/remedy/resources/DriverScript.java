d403c8f00784b919b38f3682259ddca975f5eda0
Ashish Ranjan <aranjan@remedypartners.com>
2016-05-06 18:20:33 +0530

    added support for various browsers like chrome , ff, IE and htmlUnit driver

--- src/test/java/com/remedy/resources/DriverScript.java
+++ src/test/java/com/remedy/resources/DriverScript.java
@@ -1,14 +1,10 @@
 package com.remedy.resources;
 
-import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
-
-import java.util.Arrays;
-import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 /**
  * Created by salam on 8/5/15.
@@ -24,12 +20,24 @@ public class DriverScript {
 
         public void initialize() {
             if (driver == null)
-                createNewDriverInstance();
+                createNewDriverInstance("chrome");
         }
 
-        private void createNewDriverInstance() {
-            System.setProperty("webdriver.chrome.driver", "src/test/java/com/remedy/resources/chromedriver");
-            driver = new ChromeDriver();
+        private void createNewDriverInstance(String browser) {
+        	if(browser.equalsIgnoreCase("firefox")){
+        		driver = new FirefoxDriver();
+        	}else if(browser.equalsIgnoreCase("chrome")){
+        		System.setProperty("webdriver.chrome.driver", "C:\\Users\\ashish.ranjan\\Desktop\\BrowserDrivers\\chromedriver.exe");
+                driver = new ChromeDriver();        		
+        	}else if(browser.equalsIgnoreCase("ie")){
+        		System.setProperty("webdriver.ie.driver","C:\\Users\\ashish.ranjan\\Desktop\\BrowserDrivers\\IEDriverServer.exe");
+        		driver = new InternetExplorerDriver();
+        	}else if(browser.equalsIgnoreCase("htmlUnitDriver")){
+        		driver = new HtmlUnitDriver(true);	
+        	}else{
+        		System.out.println("Browser is not correct");
+        	}
+            
         }
 
         public WebDriver getDriver() {
