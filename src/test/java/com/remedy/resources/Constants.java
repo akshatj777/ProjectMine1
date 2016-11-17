7c63c614d4d1eedfe414c9d065bf43d7b3d08a1f
Ashish Ranjan <aranjan@remedypartners.com>
2016-11-17 15:34:40 +0530

    PA-1712: parameterized aut url and super user email

--- src/test/java/com/remedy/resources/Constants.java
+++ src/test/java/com/remedy/resources/Constants.java
@@ -4,5 +4,7 @@ public class Constants {
 	
 	public static String Browser="chrome";
 	public static String OS = "mac";
+	public static String BaseUrl="https://cdn-qa.remedypartners.com/";
+	public static String SuperUserEmail="lbarinstein+qaadmin@remedypartners.com";
 
 }
