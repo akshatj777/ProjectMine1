30ba5f41c1de29cb8d0647a4d3a3de7ea9d22fae
Ashish Ranjan <aranjan@remedypartners.com>
2016-12-06 17:06:11 +0530

    PA-1722: fixed error in driver class

--- src/test/java/com/remedy/resources/DriverScript.java
+++ src/test/java/com/remedy/resources/DriverScript.java
@@ -2,6 +2,8 @@ package com.remedy.resources;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
