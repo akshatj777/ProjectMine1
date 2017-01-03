a93c6661db10c908e7b4eb60ec984c224b1be09a
Sandeep Singh <ssingh@remedypartners.com>
2017-01-03 17:37:38 +0530

    PA-1792 added test for performance claim physician report

--- src/test/java/stepDefination/Reports/ReportHomePageSteps.java
+++ src/test/java/stepDefination/Reports/ReportHomePageSteps.java
@@ -229,4 +229,9 @@ public class ReportHomePageSteps extends DriverScript {
     public void i_click_on_for_the_in_the_filter_value_list(String month, String year) throws Throwable {
     	reportHomePage.iClickOnForTheInTheFilterValueList(month, year);
     }
+    
+    @Then("^I click on \"([^\"]*)\" report text for Physician Reports$")
+    public void i_click_on_report_text_for_Physician_Reports(String text) throws Throwable {
+    	reportHomePage.iClickOnReportTextForPhysicianReports(text);
+}
 }
