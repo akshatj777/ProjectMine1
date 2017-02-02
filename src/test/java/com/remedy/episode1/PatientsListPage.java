714df45be50ce61e6fe7df3c61c5639cc4494b47
Sandeep Singh <ssingh@remedypartners.com>
2017-02-02 18:35:49 +0530

    RE-9300 added test for patient list

--- src/test/java/com/remedy/episode1/PatientsListPage.java
+++ src/test/java/com/remedy/episode1/PatientsListPage.java
@@ -20,14 +20,32 @@ public class PatientsListPage extends BaseClass {
         verifyTextForElement(driver.findElement(By.cssSelector(".ec2-embed-patient-name")), text);
 
     }
+    
+    public void iClickOnTheLeftNavigatorPresentOnTheEpisodeDashboardPage(String text){
+    	clickElement(driver.findElement(By.cssSelector("a#patientsListOpenClose")));
+    }
+    
+    public void iShouldSeeInThePatientsDropdownMenu(String text){
+    	isElementVisible(driver.findElement(By.xpath("//ul[@id='patientsFilter']//*[contains(text(),'"+text+"')]")));	
+    }
 
+    public void iClickOnInThePatientsDropdownMenu(String text){
+    	clickElement(driver.findElement(By.xpath("//ul[@id='patientsFilter']//*[contains(text(),'"+text+"')]")));
+    }
 
-
-
-
-
-
-
-
-
+    public void iClickOnPinToDashboardInToPinTheFilterToDashboard(String text){
+    	clickElement(driver.findElement(By.cssSelector("a[data-name='Inpatient Episodes']~ul>li:nth-child(2)>a")));
+    }
+    
+    public void iClickOnUnpinFromDashboardInToPinTheFilterToDashboard(String text){
+    	clickElement(driver.findElement(By.cssSelector("a[data-name='Inpatient Episodes']~ul>li:nth-child(2)>a")));
+    }
+    
+    public void iClickOnPinToBookmarksInToPinTheFilterToDashboard(String text){
+    	clickElement(driver.findElement(By.cssSelector("a[data-name='Inpatient Episodes']~ul>li:nth-child(3)>a")));
+    }
+    
+    public void iClickOnUnpinFromBookmarksInToUnpinTheFilterToDashboard(String text){
+    	clickElement(driver.findElement(By.cssSelector("a[data-name='Inpatient Episodes']~ul>li:nth-child(3)>a")));
+    }
 }
--- src/test/java/stepDefination/Episode1/PatientListPageSteps.java
+++ src/test/java/stepDefination/Episode1/PatientListPageSteps.java
@@ -3,6 +3,7 @@ package stepDefination.Episode1;
 import com.remedy.episode1.PatientsListPage;
 import com.remedy.resources.DriverScript;
 import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 
 /**
  * Created by salam on 5/22/16.
@@ -17,6 +18,43 @@ public class PatientListPageSteps extends DriverScript {
 
         patientsList.iverifyPatientListPageheader(text);
     }
+    
+    @When("^I click on \"([^\"]*)\" in the left navigator present on the episode dashboard page$")
+    public void i_click_on_in_the_left_navigator_present_on_the_episode_dashboard_page(String text) throws Throwable {
+    	 patientsList.iClickOnTheLeftNavigatorPresentOnTheEpisodeDashboardPage(text);
+    }
+
+    @Then("^I should see \"([^\"]*)\" in the Patients dropdown menu$")
+    public void i_should_see_in_the_Patients_dropdown_menu(String text) throws Throwable {
+    	 patientsList.iShouldSeeInThePatientsDropdownMenu(text);
+    }
+
+    @When("^I click on \"([^\"]*)\" in the Patients dropdown menu$")
+    public void i_click_on_in_the_Patients_dropdown_menu(String text) throws Throwable {
+    	patientsList.iClickOnInThePatientsDropdownMenu(text);
+    }
+
+
+    @Then("^I click on Pin to dashboard in \"([^\"]*)\" to pin the filter to dashboard$")
+    public void i_click_on_Pin_to_dashboard_in_to_pin_the_filter_to_dashboard(String text) throws Throwable {
+    	patientsList.iClickOnPinToDashboardInToPinTheFilterToDashboard(text);
+    }
+
+    @Then("^I click on Unpin from dashboard in \"([^\"]*)\" to unpin the filter from dashboard$")
+    public void i_click_on_Unpin_from_dashboard_in_to_unpin_the_filter_from_dashboard(String text) throws Throwable {
+    	patientsList.iClickOnUnpinFromDashboardInToPinTheFilterToDashboard(text);
+    }
+    
+    @Then("^I click on Pin to bookmarks in \"([^\"]*)\" to pin the filter to dashboard$")
+    public void i_click_on_Pin_to_bookmarks_in_to_pin_the_filter_to_dashboard(String text) throws Throwable {
+    	patientsList.iClickOnPinToBookmarksInToPinTheFilterToDashboard(text);
+    }
+
+    @Then("^I click on Unpin from bookmarks in \"([^\"]*)\" to unpin the filter from dashboard$")
+    public void i_click_on_Unpin_from_bookmarks_in_to_unpin_the_filter_from_dashboard(String text) throws Throwable {
+    	patientsList.iClickOnUnpinFromBookmarksInToUnpinTheFilterToDashboard(text);
+    }
+
 
 
 }
