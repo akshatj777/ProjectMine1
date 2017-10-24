96c6cc1d2b99abc2e65ac1d225d66143959f0022
Priyal Dubey <priyald@citiustech.com>
2017-10-24 18:00:29 +0530

    UA2 Landing page UI verification feature file

---
+++ src/test/java/stepDefination/UA2/UA2LandingPageSteps.java
@@ -0,0 +1,71 @@
+package stepDefination.UA2;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Then;
+import com.remedy.userAdmin.LandingPage;
+import com.remedy.userAdmin.LoginPage;
+import com.remedy.resources.DriverScript;
+
+public class UA2LandingPageSteps extends DriverScript{
+	LandingPage landingPage= new LandingPage(driver);
+	
+	@Then("^I should see Users link$")
+	public void i_should_see_Users_link() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^I should see Search box$")
+	public void i_should_see_Search_box() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^I should see Account status$")
+	public void i_should_see_lock_button() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^I should see Name$")
+	public void i_should_see_Name() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^I should see Role$")
+	public void i_should_see_Role() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^I should see Email$")
+	public void i_should_see_Email() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^I should see Date Created order button$")
+	public void i_should_see_Date_Created_order_button() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^I click on the top user link$")
+	public void i_click_on_the_top_user_link() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	
+	}
+
+	@Then("^I click on Users link$")
+	public void i_click_on_Users_link() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^I should see Users page$")
+	public void i_should_see_Users_page() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+}
