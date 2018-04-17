21bf1c84bf71e5f54b940b4938811ae95048e468
Deepak Bithar <dbithar@remedypartners.com>
2018-04-17 09:24:50 +0530

    Changed the naming comvention to abide by MAVEN naming convention laws

--- src/test/java/stepDefination/TestRunner.java
+++ src/test/java/stepDefination/TestRunner.java
@@ -30,7 +30,7 @@ import stepDefination.Hooks.*;
                 "junit:target/cucumber-results/cucumber-results.xml", "json:target/cucumber-results/cucumber.json",
                 "com.cucumber.listener.ExtentCucumberFormatter:target/vimalSelvam-cucumberReport/report.html"},
 
-        features = { "src/test/Features" },
+        features = { "src/test/Features/" },
 
         //glue = { "./src/test/java/stepDefination" },
         tags = {}
